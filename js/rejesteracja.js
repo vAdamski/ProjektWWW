48aeba937bbf0fb751095d9035a477643669d047
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-02-07 21:36:33 +0100

    Big update

--- js/rejesteracja.js
+++ js/rejesteracja.js
@@ -1,10 +1,10 @@
-function rejestacja(){
-    var nazwa  = document.getElementById("login").value;
-    var pwd  = document.getElementById("email").value;
-    var pwd  = document.getElementById("password").value; 
-    if(pwd == "ZAQ!2wsx"){
-    document.getElementById("p1").innerHTML = "Konto zostało utworzone.";
-    }else{
+function rejestacja() {
+    var nazwa = document.getElementById("login").value;
+    var pwd = document.getElementById("email").value;
+    var pwd = document.getElementById("password").value;
+    if (pwd == "ZAQ!2wsx") {
+        document.getElementById("p1").innerHTML = "Konto zostało utworzone.";
+    } else {
         document.getElementById("p1").innerHTML = "Konto już istnieje."
         document.getElementById("p1").style.backgroundColor = "red"
     }
